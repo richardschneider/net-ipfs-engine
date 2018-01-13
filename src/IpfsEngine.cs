2365ade88374df3356ffc9db8c8011ac4c9c6aa3
Richard Schneider <makaretu@gmail.com>
2018-01-13 17:38:47 +1300

    chore: source code and test infrastructure

---
+++ src/IpfsEngine.cs
@@ -0,0 +1,22 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using Ipfs.CoreApi;
+
+namespace Ipfs.Engine
+{
+    /// <summary>
+    ///   TODO
+    /// </summary>
+    public partial class IpfsEngine
+    {
+        static ILog log = LogManager.GetLogger(typeof(IpfsEngine));
+
+    }
+}

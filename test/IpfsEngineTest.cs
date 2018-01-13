204ffd95ad9ce037c6a6d9429b3e9d0cd65f6f3a
Richard Schneider <makaretu@gmail.com>
2018-01-13 18:31:46 +1300

    chore(ci): a test for coveralls

---
+++ test/IpfsEngineTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
+namespace Ipfs.Engine
+{
+    
+    [TestClass]
+    public class IpfsEngineTest
+    {
+        [TestMethod]
+        public void Can_Create()
+        {
+            var ipfs = new IpfsEngine();
+            Assert.IsNotNull(ipfs);
+        }
+
+    }
+}

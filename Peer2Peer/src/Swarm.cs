e9dd7bce9071751c031e297e09cdc675c02a22ce
Richard Schneider <makaretu@gmail.com>
2018-01-26 00:41:52 +1300

    feat: swarm service

---
+++ Peer2Peer/src/Swarm.cs
@@ -0,0 +1,100 @@
+﻿using Ipfs;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Peer2Peer
+{
+    /// <summary>
+    ///   Manages communication with other peers.
+    /// </summary>
+    public class Swarm : IService, IPolicy<MultiAddress>
+    {
+        /// <summary>
+        ///   Other nodes.
+        /// </summary>
+        ConcurrentBag<MultiAddress> others = new ConcurrentBag<MultiAddress>();
+
+        /// <summary>
+        ///   Get the sequence of all known peer addresses.
+        /// </summary>
+        /// <value>
+        ///   Contains any peer address that has been
+        ///   <see cref="RegisterPeerAsync">discovered</see>.
+        /// </value>
+        /// <seealso cref="RegisterPeerAsync"/>
+        public IEnumerable<MultiAddress> KnownPeerAddresses
+        {
+            get { return others; }
+        }
+
+        /// <summary>
+        ///   Register that a peer's address has been discovered.
+        /// </summary>
+        /// <param name="address">
+        ///   An address to the peer. 
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///    A task that represents the asynchronous get operation. The task's result
+        ///    is <b>true</b> if the <paramref name="address"/> is registered.
+        /// </returns>
+        /// <remarks>
+        ///   If the <paramref name="address"/> is not already known, then it is
+        ///   added to the <see cref="KnownPeerAddresses"/> unless the <see cref="BlackList"/>
+        ///   or <see cref="WhiteList"/> policies forbid it.
+        /// </remarks>
+        public async Task<bool> RegisterPeerAsync(MultiAddress address, CancellationToken cancel = default(CancellationToken))
+        {
+            if (await IsAllowedAsync(address, cancel) && !others.Contains(address))
+            {
+                others.Add(address);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///   The addresses that cannot be used.
+        /// </summary>
+        public BlackList<MultiAddress> BlackList { get; } = new BlackList<MultiAddress>();
+
+        /// <summary>
+        ///   The addresses that can be used.
+        /// </summary>
+        public WhiteList<MultiAddress> WhiteList { get; } = new WhiteList<MultiAddress>();
+
+        /// <inheritdoc />
+        public Task StartAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        public Task StopAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> IsAllowedAsync(MultiAddress target, CancellationToken cancel = default(CancellationToken))
+        {
+            return await BlackList.IsAllowedAsync(target, cancel)
+                && await WhiteList.IsAllowedAsync(target, cancel);
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> IsNotAllowedAsync(MultiAddress target, CancellationToken cancel = default(CancellationToken))
+        {
+            var q = await IsAllowedAsync(target, cancel);
+            return !q;
+        }
+    }
+}
---
+++ Peer2Peer/test/SwarmTest.cs
@@ -0,0 +1,50 @@
+﻿using Ipfs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Peer2Peer
+{
+    [TestClass]
+    public class SwarmTest
+    {
+        MultiAddress mars = "/ip4/10.1.10.10/tcp/29087/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3";
+        MultiAddress venus = "/ip6/2604:a880:800:10::4a:5001/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs6";
+
+        [TestMethod]
+        public async Task NewPeerAddress()
+        {
+            var swarm = new Swarm();
+            await swarm.RegisterPeerAsync(mars);
+            Assert.IsTrue(swarm.KnownPeerAddresses.Contains(mars));
+        }
+
+        [TestMethod]
+        public void NewPeerAddress_BlackList()
+        {
+            var swarm = new Swarm();
+            swarm.BlackList.Add(mars);
+
+            Assert.IsFalse(swarm.RegisterPeerAsync(mars).Result);
+            Assert.IsFalse(swarm.KnownPeerAddresses.Contains(mars));
+
+            Assert.IsTrue(swarm.RegisterPeerAsync(venus).Result);
+            Assert.IsTrue(swarm.KnownPeerAddresses.Contains(venus));
+        }
+
+        [TestMethod]
+        public void NewPeerAddress_WhiteList()
+        {
+            var swarm = new Swarm();
+            swarm.WhiteList.Add(venus);
+
+            Assert.IsFalse(swarm.RegisterPeerAsync(mars).Result);
+            Assert.IsFalse(swarm.KnownPeerAddresses.Contains(mars));
+
+            Assert.IsTrue(swarm.RegisterPeerAsync(venus).Result);
+            Assert.IsTrue(swarm.KnownPeerAddresses.Contains(venus));
+        }
+    }
+}

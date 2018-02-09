ed0cb2df01cb45b42ff828273155e3d1f3953506
Richard Schneider <makaretu@gmail.com>
2018-02-09 15:13:35 +1300

    feat: Connect to UDP endpoint

---
+++ Peer2Peer/src/Transports/DatagramStream.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Peer2Peer.Transports
+{
+    class DatagramStream : Stream
+    {
+        Socket socket;
+        bool ownsSocket;
+        MemoryStream sendBuffer = new MemoryStream();
+        MemoryStream receiveBuffer = new MemoryStream();
+        byte[] datagram = new byte[2048];
+
+        public DatagramStream(Socket socket, bool ownsSocket = false)
+        {
+            this.socket = socket;
+            this.ownsSocket = ownsSocket;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                try
+                {
+                    Flush();
+                }
+                catch (SocketException)
+                {
+                    // eat it
+                }
+            }
+            if (ownsSocket && socket != null)
+            {
+                try
+                {
+                    socket.Dispose();
+                }
+                catch (SocketException)
+                {
+                    // eat it
+                }
+                finally
+                {
+                    socket = null;
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => throw new NotSupportedException();
+
+        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+
+        public override void Flush()
+        {
+            FlushAsync().Wait();
+        }
+
+        public override async Task FlushAsync(CancellationToken cancellationToken)
+        {
+            if (sendBuffer.Position > 0)
+            {
+                var bytes = new ArraySegment<byte>(sendBuffer.ToArray());
+                sendBuffer.Position = 0;
+                await socket.SendAsync(bytes, SocketFlags.None);
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return ReadAsync(buffer, offset, count).Result;
+        }
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            // If no data.
+            if (receiveBuffer.Position == receiveBuffer.Length)
+            {
+                await FlushAsync();
+                receiveBuffer.Position = 0;
+                receiveBuffer.SetLength(0);
+                var size = socket.Receive(datagram);
+                receiveBuffer.Write(datagram, 0, size);
+                receiveBuffer.Position = 0;
+            }
+            return receiveBuffer.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            sendBuffer.Write(buffer, offset, count);
+        }
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            Write(buffer, offset, count);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Peer2Peer/src/Transports/Udp.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Ipfs;
+using System.Net.Sockets;
+using Common.Logging;
+using System.Net;
+
+namespace Peer2Peer.Transports
+{
+    /// <summary>
+    ///   Establishes a duplex stream between two peers
+    ///   over UDP.
+    /// </summary>
+    public class Udp : IPeerTransport
+    {
+        static ILog log = LogManager.GetLogger(typeof(Udp));
+
+        /// <inheritdoc />
+        public async Task<Stream> ConnectAsync(MultiAddress address, CancellationToken cancel = default(CancellationToken))
+        {
+            var port = address.Protocols
+                .Where(p => p.Name == "udp")
+                .Select(p => Int32.Parse(p.Value))
+                .First();
+            var ip = address.Protocols
+                .Where(p => p.Name == "ip4" || p.Name == "ip6")
+                .First();
+            var socket = new Socket(
+                ip.Name == "ip4" ? AddressFamily.InterNetwork : AddressFamily.InterNetworkV6,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+
+            // Handle cancellation of the connect attempt
+            cancel.Register(() =>
+            {
+                socket.Dispose();
+                socket = null;
+            });
+
+            try
+            {
+                log.Debug("connecting to " + address);
+                await socket.ConnectAsync(ip.Value, port);
+                log.Debug("connected " + address);
+            }
+            catch (Exception) when (cancel.IsCancellationRequested)
+            {
+                // eat it, the caller has cancelled and doesn't care.
+            }
+            catch (Exception e)
+            {
+                log.Warn("failed " + address, e);
+                throw;
+            }
+            if (cancel.IsCancellationRequested)
+            {
+                log.Debug("cancel " + address);
+                if (socket != null)
+                {
+                    socket.Dispose();
+                }
+                return null;
+            }
+            return new DatagramStream(socket, ownsSocket: true);
+        }
+
+        /// <inheritdoc />
+        public MultiAddress Listen(MultiAddress address, Action<Stream, MultiAddress, MultiAddress> handler, CancellationToken cancel)
+        {
+            var port = address.Protocols
+                .Where(p => p.Name == "udp")
+                .Select(p => Int32.Parse(p.Value))
+                .FirstOrDefault();
+            var ip = address.Protocols
+                .Where(p => p.Name == "ip4" || p.Name == "ip6")
+                .First();
+            var ipAddress = IPAddress.Parse(ip.Value);
+            var endPoint = new IPEndPoint(ipAddress, port);
+            var socket = new Socket(
+                endPoint.AddressFamily,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+            socket.Bind(endPoint);
+
+            // If no port specified, then add it.
+            var actualPort = ((IPEndPoint)socket.LocalEndPoint).Port;
+            if (port != actualPort)
+            {
+                address = address.Clone();
+                var protocol = address.Protocols.FirstOrDefault(p => p.Name == "udp");
+                if (protocol != null)
+                {
+                    protocol.Value = actualPort.ToString();
+                }
+                else
+                {
+                    address.Protocols.AddRange(new MultiAddress("/udp/" + actualPort).Protocols);
+                }
+            }
+
+            // TODO: UDP listener
+            throw new NotImplementedException();
+#if false
+            var stream = new DatagramStream(socket, ownsSocket: true);
+            handler(stream, address, null);
+
+            return address;
+#endif
+        }
+
+    }
+}

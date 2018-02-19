d7baf106f737b84555eb75349c16eaa606a2e495
Richard Schneider <makaretu@gmail.com>
2018-02-20 11:32:46 +1300

    feat: PeerConnection

---
+++ Peer2Peer/src/PeerConnection.cs
@@ -0,0 +1,169 @@
+﻿using Common.Logging;
+using Ipfs;
+using Peer2Peer.Protocols;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Peer2Peer
+{
+    /// <summary>
+    ///   A connection between two peers.
+    /// </summary>
+    public class PeerConnection : IDisposable
+    {
+        static ILog log = LogManager.GetLogger(typeof(PeerConnection));
+
+        /// <summary>
+        ///   The local peer.
+        /// </summary>
+        public Peer LocalPeer { get; set; }
+
+        /// <summary>
+        ///   The remote peer.
+        /// </summary>
+        public Peer RemotePeer { get; set; }
+
+        /// <summary>
+        ///   The local peer's end point.
+        /// </summary>
+        public MultiAddress LocalAddress { get; set; }
+
+        /// <summary>
+        ///   The remote peer's end point.
+        /// </summary>
+        public MultiAddress RemoteAddress { get; set; }
+
+        /// <summary>
+        ///   The duplex stream between the two peers.
+        /// </summary>
+        public Stream Stream { get; set; }
+
+        /// <summary>
+        ///   Signals that the security for the connection is established.
+        /// </summary>
+        /// <remarks>
+        ///   This can be awaited.
+        /// </remarks>
+        public TaskCompletionSource<bool> SecurityEstablished { get; }  = new TaskCompletionSource<bool>();
+
+        /// <summary>
+        ///  Establish the connection with the remote node.
+        /// </summary>
+        /// <param name="cancel"></param>
+        /// <returns></returns>
+        /// <remarks>
+        ///   This should be called when the local peer wants a connection with
+        ///   the remote peer.
+        /// </remarks>
+        public async Task<Peer> InitiateAsync(CancellationToken cancel = default(CancellationToken))
+        {
+            await EstablishProtocolAsync("/multistream/", cancel);
+            await EstablishProtocolAsync("/plaintext/", cancel);
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            new Multistream1().ProcessRequestAsync(this, cancel);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            return null;
+        }
+
+        async Task EstablishProtocolAsync(string name, CancellationToken cancel)
+        {
+            var protocols = ProtocolRegistry.Protocols.Keys
+                .Where(k => k.StartsWith(name))
+                .Select(k => VersionedName.Parse(k))
+                .OrderByDescending(vn => vn)
+                .Select(vn => vn.ToString());
+            foreach (var protocol in protocols)
+            {
+                await Message.WriteAsync(protocol, Stream, cancel);
+                var result = await Message.ReadStringAsync(Stream, cancel);
+                if (result == protocol)
+                {
+                    log.Debug("waiting for response " + protocol);
+                    await ProtocolRegistry.Protocols[protocol].ProcessResponseAsync(this, cancel);
+                    log.Debug("dones response " + protocol);
+                    return;
+                }
+            }
+            throw new Exception($"Remote does not support protocol '{name}'.");
+        }
+
+        /// <summary>
+        ///   Accept a connection from the remote peer.
+        /// </summary>
+        /// <param name="cancel"></param>
+        /// <returns></returns>
+        /// <remarks>
+        ///   This should be called when a remote peer is connecting to the
+        ///   local peer.
+        /// </remarks>
+        public async Task RespondAsync(CancellationToken cancel = default(CancellationToken))
+        {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            new Multistream1().ProcessRequestAsync(this, cancel);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            log.Debug("waiting for establishment of security from " + RemoteAddress);
+            await SecurityEstablished.Task;
+            log.Debug("security is okay");
+            // TODO: Get remote identity and return the Peer
+            //await EstablishProtocolAsync("/ipfs/id/", cancel);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        /// <summary>
+        ///  TODO
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (Stream != null)
+                    {
+                        try
+                        {
+                            Stream.Dispose();
+                        }
+                        finally
+                        {
+                            Stream = null;
+                        }
+                    }
+                }
+
+                // free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~PeerConnection() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+       /// <summary>
+       /// 
+       /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ Peer2Peer/test/PeerConnectionTest.cs
@@ -0,0 +1,28 @@
+﻿using Ipfs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Peer2Peer
+{
+    [TestClass]
+    public class PeerConnectionTest
+    {
+        [TestMethod]
+        public void Disposing()
+        {
+            var stream = new MemoryStream();
+            var connection = new PeerConnection { Stream = stream };
+            Assert.AreEqual(stream, connection.Stream);
+
+            connection.Dispose();
+            Assert.IsNull(connection.Stream);
+
+            // Can be disposed multiple times.
+            connection.Dispose();
+        }
+    }
+}

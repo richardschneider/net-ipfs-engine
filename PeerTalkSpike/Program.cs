2c891f045929eac70ab8d29abe0c84287a49a799
Richard Schneider <makaretu@gmail.com>
2018-02-21 13:40:59 +1300

    test: spike on unix

--- PeerTalkSpike/Program.cs
+++ PeerTalkSpike/Program.cs
@@ -1,10 +1,19 @@
-﻿using Peer2Peer;
+﻿using Common.Logging;
+using Common.Logging.Simple;
+using Ipfs;
+using Peer2Peer;
 using Peer2Peer.Protocols;
 using Peer2Peer.Transports;
 using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PeerTalkSpike
 {
@@ -12,10 +21,29 @@ namespace PeerTalkSpike
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Debug.AutoFlush = true;
+            
+            // set logger factory
+            var properties = new Common.Logging.Configuration.NameValueCollection();
+            properties["level"] = "TRACE";
+            properties["showLogName"] = "true";
+            properties["showDateTime"] = "true";
+            LogManager.Adapter = new ConsoleOutLoggerFactoryAdapter(properties);
 
-            ClientConnect();
+            // obtain logger instance
+            ILog log = LogManager.GetCurrentClassLogger();
+
+            // log something
+            log.Debug("Some Debug Log Output");
+
+            //ClientConnect();
+
             //ServerListen();
+
+            var t = new Test();
+            t.SendReceive().Wait();
+            Console.WriteLine("finished");
+            Console.ReadKey();
         }
         
         static void ServerListen()
@@ -54,14 +82,67 @@ namespace PeerTalkSpike
             var tcp = new Tcp();
             var peer = tcp.ConnectAsync("/ip4/127.0.0.1/tcp/4002").Result;
 
-            var got = Message.ReadStringAsync(peer).Result;
             Message.WriteAsync("/multistream/1.0.0", peer).Wait();
+            var got = Message.ReadStringAsync(peer).Result;
             Message.WriteAsync("/plaintext/1.0.0", peer).Wait();
+            got = Message.ReadStringAsync(peer).Result;
+
+            Message.WriteAsync("/multistream/1.0.0", peer).Wait();
+            got = Message.ReadStringAsync(peer).Result;
+
+            //Message.WriteAsync("/ipfs/id/1.0.0", peer).Wait();
+            //got = Message.ReadStringAsync(peer).Result;
+            //new Identify1().ProcessMessagesAsync(peer).Wait();
             while (true)
             {
-                var msg = Message.ReadStringAsync(peer).Result;
-                Message.WriteAsync(msg, peer).Wait();
+                var b = peer.ReadByte();
+                if (b == -1) break;
+                Console.WriteLine("got {0:x2} '{1}'", b, (char)b);
             }
         }
+    }
+
+    class Test
+    {
+        public async Task SendReceive()
+        {
+            var cs = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            var tcp = new Tcp();
+            using (var server = new HelloServer())
+            using (var stream = await tcp.ConnectAsync(server.Address, cs.Token))
+            {
+                var bytes = new byte[5];
+                await stream.ReadAsync(bytes, 0, bytes.Length);
+                Console.WriteLine("got " + Encoding.UTF8.GetString(bytes));
+                //Assert.AreEqual("hello", Encoding.UTF8.GetString(bytes));
+            }
+        }
+
+        class HelloServer : IDisposable
+        {
+            CancellationTokenSource cs = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+
+            public HelloServer()
+            {
+                var tcp = new Tcp();
+                Address = tcp.Listen("/ip4/127.0.0.1", Handler, cs.Token);
+            }
+
+            public MultiAddress Address { get; set; }
+
+            public void Dispose()
+            {
+                cs.Cancel();
+            }
+
+            void Handler(Stream stream, MultiAddress local, MultiAddress remote)
+            {
+                var msg = Encoding.UTF8.GetBytes("hello");
+                stream.Write(msg, 0, msg.Length);
+                stream.Flush();
+                stream.Dispose();
+            }
+        }
+
     }
 }

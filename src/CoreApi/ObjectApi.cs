25a8b3b3ba89690a3450fe0980d4d32fca1f80fa
Richard Schneider <makaretu@gmail.com>
2018-03-09 18:05:32 +1300

    feat(ObjectApi): implement

--- src/CoreApi/ObjectApi.cs
+++ src/CoreApi/ObjectApi.cs
@@ -5,21 +5,40 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Ipfs.CoreApi;
+using Ipfs.Engine.UnixFileSystem;
 
 namespace Ipfs.Engine.CoreApi
 {
     class ObjectApi : IObjectApi
     {
+        static DagNode EmptyNode;
+        static DagNode EmptyDirectory;
+
         IpfsEngine ipfs;
+
+        static ObjectApi()
+        {
+            EmptyNode = new DagNode(new byte[0]);
+            var _ = EmptyNode.Id;
+
+            var dm = new DataMessage { Type = DataType.Directory };
+            using (var pb = new MemoryStream())
+            {
+                ProtoBuf.Serializer.Serialize<DataMessage>(pb, dm);
+                EmptyDirectory = new DagNode(pb.ToArray());
+            }
+            _ = EmptyDirectory.Id;
+        }
 
         public ObjectApi(IpfsEngine ipfs)
         {
             this.ipfs = ipfs;
         }
 
-        public Task<Stream> DataAsync(Cid id, CancellationToken cancel = default(CancellationToken))
+        public async Task<Stream> DataAsync(Cid id, CancellationToken cancel = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            var node = await GetAsync(id, cancel);
+            return node.DataStream;
         }
 
         public async Task<DagNode> GetAsync(Cid id, CancellationToken cancel = default(CancellationToken))
@@ -28,29 +47,40 @@ namespace Ipfs.Engine.CoreApi
             return new DagNode(block.DataStream);
         }
 
-        public Task<IEnumerable<IMerkleLink>> LinksAsync(Cid id, CancellationToken cancel = default(CancellationToken))
+        public async Task<IEnumerable<IMerkleLink>> LinksAsync(Cid id, CancellationToken cancel = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            var node = await GetAsync(id, cancel);
+            return node.Links;
         }
 
         public Task<DagNode> NewAsync(string template = null, CancellationToken cancel = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            switch (template)
+            {
+                case null:
+                    return Task.FromResult(EmptyNode);
+                case "unixfs-dir":
+                    return Task.FromResult(EmptyDirectory);
+                default:
+                    throw new ArgumentException($"Unknown template '{template}'.", "template");
+            }
         }
 
         public Task<DagNode> NewDirectoryAsync(CancellationToken cancel = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            return Task.FromResult(EmptyDirectory);
         }
 
         public Task<DagNode> PutAsync(byte[] data, IEnumerable<IMerkleLink> links = null, CancellationToken cancel = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            var node = new DagNode(data, links);
+            return PutAsync(node, cancel);
         }
 
-        public Task<DagNode> PutAsync(DagNode node, CancellationToken cancel = default(CancellationToken))
+        public async Task<DagNode> PutAsync(DagNode node, CancellationToken cancel = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            node.Id = await ipfs.Block.PutAsync(node.ToArray(), cancel: cancel);
+            return node;
         }
     }
 }
---
+++ test/CoreApi/ObjectApiTest.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ipfs.Engine.CoreApi
+{
+
+    [TestClass]
+    public class ObjectApiTest
+    {
+        IpfsEngine ipfs = TestFixture.Ipfs;
+
+        [TestMethod]
+        public async Task New_Template_Null()
+        {
+            var node = await ipfs.Object.NewAsync();
+            Assert.AreEqual("QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n", (string)node.Id);
+        }
+
+        [TestMethod]
+        public async Task New_Template_UnixfsDir()
+        {
+            var node = await ipfs.Object.NewAsync("unixfs-dir");
+            Assert.AreEqual("QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn", (string)node.Id);
+
+            node = await ipfs.Object.NewDirectoryAsync();
+            Assert.AreEqual("QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn", (string)node.Id);
+        }
+
+        [TestMethod]
+        public void New_Template_Unknown()
+        {
+            ExceptionAssert.Throws<Exception>(() =>
+            {
+                var node = ipfs.Object.NewAsync("unknown-template").Result;
+            });
+        }
+
+        [TestMethod]
+        public async Task Put_Get_Dag()
+        {
+            var adata = Encoding.UTF8.GetBytes("alpha");
+            var bdata = Encoding.UTF8.GetBytes("beta");
+            var alpha = new DagNode(adata);
+            var beta = new DagNode(bdata, new[] { alpha.ToLink() });
+            var x = await ipfs.Object.PutAsync(beta);
+            var node = await ipfs.Object.GetAsync(x.Id);
+            CollectionAssert.AreEqual(beta.DataBytes, node.DataBytes);
+            Assert.AreEqual(beta.Links.Count(), node.Links.Count());
+            Assert.AreEqual(beta.Links.First().Id, node.Links.First().Id);
+            Assert.AreEqual(beta.Links.First().Name, node.Links.First().Name);
+            Assert.AreEqual(beta.Links.First().Size, node.Links.First().Size);
+        }
+
+        [TestMethod]
+        public async Task Put_Get_Data()
+        {
+            var adata = Encoding.UTF8.GetBytes("alpha");
+            var bdata = Encoding.UTF8.GetBytes("beta");
+            var alpha = new DagNode(adata);
+            var beta = await ipfs.Object.PutAsync(bdata, new[] { alpha.ToLink() });
+            var node = await ipfs.Object.GetAsync(beta.Id);
+            CollectionAssert.AreEqual(beta.DataBytes, node.DataBytes);
+            Assert.AreEqual(beta.Links.Count(), node.Links.Count());
+            Assert.AreEqual(beta.Links.First().Id, node.Links.First().Id);
+            Assert.AreEqual(beta.Links.First().Name, node.Links.First().Name);
+            Assert.AreEqual(beta.Links.First().Size, node.Links.First().Size);
+        }
+
+        [TestMethod]
+        public async Task Data()
+        {
+            var adata = Encoding.UTF8.GetBytes("alpha");
+            var node = await ipfs.Object.PutAsync(adata);
+            using (var stream = await ipfs.Object.DataAsync(node.Id))
+            {
+                var bdata = new byte[adata.Length];
+                stream.Read(bdata, 0, bdata.Length);
+                CollectionAssert.AreEqual(adata, bdata);
+            }
+        }
+
+        [TestMethod]
+        public async Task Links()
+        {
+            var adata = Encoding.UTF8.GetBytes("alpha");
+            var bdata = Encoding.UTF8.GetBytes("beta");
+            var alpha = new DagNode(adata);
+            var beta = await ipfs.Object.PutAsync(bdata, new[] { alpha.ToLink() });
+            var links = await ipfs.Object.LinksAsync(beta.Id);
+            Assert.AreEqual(beta.Links.Count(), links.Count());
+            Assert.AreEqual(beta.Links.First().Id, links.First().Id);
+            Assert.AreEqual(beta.Links.First().Name, links.First().Name);
+            Assert.AreEqual(beta.Links.First().Size, links.First().Size);
+        }
+
+#if false
+        [TestMethod]
+        public async Task Stat()
+        {
+            var data1 = Encoding.UTF8.GetBytes("Some data 1");
+            var data2 = Encoding.UTF8.GetBytes("Some data 2");
+            var node2 = new DagNode(data2);
+            var node1 = await ipfs.Object.PutAsync(data1,
+                new[] { node2.ToLink("some-link") });
+            var info = await ipfs.Object.StatAsync(node1.Id);
+            Assert.AreEqual("QmPR7W4kaADkAo4GKEVVPQN81EDUFCHJtqejQZ5dEG7pBC", info.Hash);
+            Assert.AreEqual(1, info.NumLinks);
+            Assert.AreEqual(64, info.BlockSize);
+            Assert.AreEqual(53, info.LinksSize);
+            Assert.AreEqual(11, info.DataSize);
+            Assert.AreEqual(77, info.CumulativeSize);
+        }
+#endif
+
+        [TestMethod]
+        [Ignore("Waiting for bitswap")]
+        public async Task Get_Nonexistent()
+        {
+            var data = Encoding.UTF8.GetBytes("Some data for net-ipfs-engine-test that cannot be found");
+            var node = new DagNode(data);
+            var id = node.Id;
+            var cs = new CancellationTokenSource(500);
+            try
+            {
+                var _ = await ipfs.Object.GetAsync(id, cs.Token);
+                Assert.Fail("Did not throw TaskCanceledException");
+            }
+            catch (TaskCanceledException)
+            {
+                return;
+            }
+        }
+
+    }
+}

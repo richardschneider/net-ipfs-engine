f6483da1a8bbb4a97fc6cce9a8eb640d0573a390
Richard Schneider <makaretu@gmail.com>
2018-03-13 15:51:11 +1300

    test: ProtobufTest and ArraySegment

---
+++ test/ProtobufTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProtoBuf;
+using System;
+using System.IO;
+
+namespace ProtobufTesting
+{
+    [ProtoContract]
+    class M1
+    {
+        [ProtoMember(1, IsRequired = false)]
+        public byte[] Data;
+    }
+
+    [ProtoContract]
+    class M2
+    {
+        [ProtoMember(1, IsRequired = false)]
+        public ArraySegment<byte>? Data;
+    }
+
+    [TestClass]
+    public class ProtobufTest
+    {
+        [TestMethod]
+        public void NullData()
+        {
+            var m1 = new M1();
+            var ms1 = new MemoryStream();
+            Serializer.Serialize<M1>(ms1, m1);
+            var bytes1 = ms1.ToArray();
+
+            var m2 = new M2();
+            var ms2 = new MemoryStream();
+            Serializer.Serialize<M2>(ms2, m2);
+            var bytes2 = ms2.ToArray();
+
+            CollectionAssert.AreEqual(bytes1, bytes2);
+        }
+
+        [TestMethod]
+        [Ignore("https://github.com/mgravell/protobuf-net/issues/368")]
+        public void SomeData()
+        {
+            var data = new byte[] { 10, 11, 12 };
+            var m1 = new M1 { Data = data };
+            var ms1 = new MemoryStream();
+            Serializer.Serialize<M1>(ms1, m1);
+            var bytes1 = ms1.ToArray();
+
+            var m2 = new M2 { Data = new ArraySegment<byte>(data) };
+            var ms2 = new MemoryStream();
+            Serializer.Serialize<M2>(ms2, m2);
+            var bytes2 = ms2.ToArray();
+
+            CollectionAssert.AreEqual(bytes1, bytes2);
+        }
+    }
+}

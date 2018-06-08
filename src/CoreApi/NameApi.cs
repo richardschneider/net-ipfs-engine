ab016af3f41fd02c4a60e2e00e75e09feaf1df98
Richard Schneider <makaretu@gmail.com>
2018-06-08 14:36:37 +1200

    feat(NameApi): resolve dnslink #18

--- src/CoreApi/NameApi.cs
+++ src/CoreApi/NameApi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -26,9 +27,51 @@ namespace Ipfs.Engine.CoreApi
             throw new NotImplementedException();
         }
 
-        public Task<string> ResolveAsync(string name, bool recursive = false, bool nocache = false, CancellationToken cancel = default(CancellationToken))
+        public async Task<string> ResolveAsync(string name, bool recursive = false, bool nocache = false, CancellationToken cancel = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            do
+            {
+                if (name.StartsWith("/ipns/"))
+                {
+                    name = name.Substring(6);
+                }
+                var parts = name.Split('/').Where(p => p.Length > 0).ToArray();
+                if (parts.Length == 0)
+                    throw new ArgumentException($"Cannot resolve '{name}'.");
+                if (IsDomainName(parts[0]))
+                {
+                    name = await ipfs.Dns.ResolveAsync(parts[0]);
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+                if (parts.Length > 1)
+                {
+                    name = name + "/" + string.Join("/", parts, 1, parts.Length - 1);
+                }
+            } while (recursive && !name.StartsWith("/ipfs/"));
+
+            return name;
         }
+
+        /// <summary>
+        ///   Determines if the supplied string is a valid domain name.
+        /// </summary>
+        /// <param name="name">
+        ///   An domain name, such as "ipfs.io".
+        /// </param>
+        /// <returns>
+        ///   <b>true</b> if <paramref name="name"/> is a domain name;
+        ///   otherwise, <b>false</b>.
+        /// </returns>
+        /// <remarks>
+        ///    A domain must contain at least one '.'.
+        /// </remarks>
+        public static bool IsDomainName(string name)
+        {
+            return name.IndexOf('.') > 0;
+        }
+
     }
 }
---
+++ test/CoreApi/NameApiTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipfs.Engine
+{
+
+    [TestClass]
+    public class NameApiTest
+    {
+        IpfsEngine ipfs = TestFixture.Ipfs;
+
+        [TestMethod]
+        public async Task Resolve_DnsLink()
+        {
+            var iopath = await ipfs.Name.ResolveAsync("ipfs.io");
+            StringAssert.StartsWith(iopath, "/ipfs/");
+
+            var path = await ipfs.Name.ResolveAsync("/ipns/ipfs.io");
+            Assert.AreEqual(iopath, path);
+
+            path = await ipfs.Name.ResolveAsync("/ipns/ipfs.io/media/x");
+            Assert.AreEqual(iopath + "/media/x", path);
+        }
+
+        [TestMethod]
+        public void Resolve_NoDnsLink()
+        {
+            ExceptionAssert.Throws<Exception>(() =>
+            {
+                var _ = ipfs.Dns.ResolveAsync("google.com").Result;
+            });
+        }
+    }
+}

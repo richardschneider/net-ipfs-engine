2743de73a7be9e7451963bf0b0abf7e70b31dc78
Richard Schneider <makaretu@gmail.com>
2018-10-09 22:05:24 +1300

    feat: a generic file store

---
+++ src/FileStore.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ipfs.Engine
+{
+    /// <summary>
+    ///   A repository for name value pairs.
+    /// </summary>
+    /// <typeparam name="TName">
+    ///   The type used for a unique name.
+    /// </typeparam>
+    /// <typeparam name="TValue">
+    ///   The type used for the value.
+    /// </typeparam>
+    public class FileStore<TName, TValue>
+        where TValue : class
+    {
+
+        /// <summary>
+        ///   The fully qualififed path to a directory
+        ///   that stores the name value pairs.
+        /// </summary>
+        /// <value>
+        ///   A fully qualified path.
+        /// </value>
+        /// <remarks>
+        ///   The directory must already exist.
+        /// </remarks>
+        public string Folder { get; set; }
+
+        /// <summary>
+        ///   A function that converts the name to a case insensitive key name.
+        /// </summary>
+        public Func<TName, string> NameToKey { get; set; }
+
+        /// <summary>
+        ///   Sends the value to the stream.
+        /// </summary>
+        public Func<Stream, TValue, Task> Serialize { get; set; }
+
+        /// <summary>
+        ///   Retrieves the value from the stream.
+        /// </summary>
+        public Func<Stream, Task<TValue>> Deserialize { get; set; }
+
+        /// <summary>
+        ///   Try to get the value with the specified name.
+        /// </summary>
+        /// <param name="name">
+        ///   The unique name of the entity.
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous operation. The task's result is
+        ///   a <typeparamref name="TValue"/> or <b>null</b> if the <paramref name="name"/>
+        ///   does not exist.
+        /// </returns>
+        public async Task<TValue> TryGetAsync(TName name, CancellationToken cancel = default(CancellationToken))
+        {
+            var path = GetPath(name);
+            if (!File.Exists(path))
+            {
+                return null;
+            }
+
+            using (var content = File.OpenRead(path))
+            {
+                return await Deserialize(content);
+            }
+        }
+
+        /// <summary>
+        ///   Get the value with the specified name.
+        /// </summary>
+        /// <param name="name">
+        ///   The unique name of the entity.
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous operation. The task's result is
+        ///   a <typeparamref name="TValue"/>
+        /// </returns>
+        /// <exception cref="KeyNotFoundException">
+        ///   When the <paramref name="name"/> does not exist.
+        /// </exception>
+        public async Task<TValue> GetAsync(TName name, CancellationToken cancel = default(CancellationToken))
+        {
+            var value = await TryGetAsync(name, cancel);
+            if (value == null)
+                throw new KeyNotFoundException($"Missing '{name}'.");
+
+            return value;
+        }
+
+        /// <summary>
+        ///   Put the value with the specified name.
+        /// </summary>
+        /// <param name="name">
+        ///   The unique name of the entity.
+        /// </param>
+        /// <param name="value">
+        ///   The entity.
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous operation.
+        /// </returns>
+        /// <remarks>
+        ///   If <paramref name="name"/> already exists, it's value is overwriten.
+        /// </remarks>
+        public async Task PutAsync (TName name, TValue value, CancellationToken cancel = default(CancellationToken))
+        {
+            var path = GetPath(name);
+
+            using (var stream = File.Create(path))
+            {
+                await Serialize(stream, value);
+            }
+        }
+
+        /// <summary>
+        ///   Remove the value with the specified name.
+        /// </summary>
+        /// <param name="name">
+        ///   The unique name of the entity.
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous operation.
+        /// </returns>
+        /// <remarks>
+        ///   A non-existent <paramref name="name"/> does nothing.
+        /// </remarks>
+        public Task RemoveAsync(TName name, CancellationToken cancel = default(CancellationToken))
+        {
+            var path = GetPath(name);
+            File.Delete(path);
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        ///   Get's the serialised length of the entity.
+        /// </summary>
+        /// <param name="name">
+        ///   The unique name of the entity.
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous operation. The task's result is
+        ///   a nullable long.
+        /// </returns>
+        /// <remarks>
+        ///   Return a null when the <paramref name="name"/> does not exist.
+        /// </remarks>
+        public Task<long?> LengthAsync(TName name, CancellationToken cancel = default(CancellationToken))
+        {
+            var path = GetPath(name);
+            var fi = new FileInfo(path);
+            long? length = null;
+            if (fi.Exists)
+                length = fi.Length;
+            return Task.FromResult(length);
+        }
+
+        /// <summary>
+        ///   Local file system path of the name.
+        /// </summary>
+        string GetPath(TName name)
+        {
+            return Path.Combine(Folder, NameToKey(name));
+        }
+
+
+    }
+}
---
+++ test/FileStoreTest.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Ipfs.Engine
+{
+    [TestClass]
+    public class FileStoreTest
+    {
+        class Entity
+        {
+            public int Number;
+            public string Value;
+        }
+
+        Entity a = new Entity { Number = 1, Value = "a" };
+        Entity b = new Entity { Number = 2, Value = "b" };
+
+        FileStore<int, Entity> Store
+        {
+            get
+            {
+                var folder = Path.Combine(TestFixture.Ipfs.Options.Repository.Folder, "test-filestore");
+                if (!Directory.Exists(folder))
+                    Directory.CreateDirectory(folder);
+
+                return new FileStore<int, Entity>
+                {
+                    Folder = folder,
+                    NameToKey = name => name.ToString(),
+                    Serialize = (s, e) =>
+                    {
+                        var x = JsonConvert.SerializeObject(e);
+                        var b = Encoding.UTF8.GetBytes(x);
+                        s.Write(b, 0, b.Length);
+                        return Task.CompletedTask;
+                    },
+                    Deserialize = (s) =>
+                    {
+                        var buffer = new byte[1024];
+                        var n = s.Read(buffer, 0, buffer.Length);
+                        var b = Encoding.UTF8.GetString(buffer, 0, n);
+                        return Task.FromResult(JsonConvert.DeserializeObject<Entity>(b));
+                    }
+                };
+            }
+        }
+
+        [TestMethod]
+        public async Task PutAndGet()
+        {
+            var store = Store;
+
+            await store.PutAsync(a.Number, a);
+            await store.PutAsync(b.Number, b);
+
+            var a1 = await store.GetAsync(a.Number);
+            Assert.AreEqual(a.Number, a1.Number);
+            Assert.AreEqual(a.Value, a1.Value);
+
+            var b1 = await store.GetAsync(b.Number);
+            Assert.AreEqual(b.Number, b1.Number);
+            Assert.AreEqual(b.Value, b1.Value);
+        }
+
+        [TestMethod]
+        public async Task TryGet()
+        {
+            var store = Store;
+            await store.PutAsync(3, a);
+            var a1 = await store.GetAsync(3);
+            Assert.AreEqual(a.Number, a1.Number);
+            Assert.AreEqual(a.Value, a1.Value);
+
+            var a3 = await store.TryGetAsync(42);
+            Assert.IsNull(a3);
+        }
+
+        [TestMethod]
+        public void Get_Unknown()
+        {
+            var store = Store;
+
+            ExceptionAssert.Throws<KeyNotFoundException>(() =>
+            {
+                var _ = Store.GetAsync(42).Result;
+            });
+        }
+
+        [TestMethod]
+        public async Task Remove()
+        {
+            var store = Store;
+            await store.PutAsync(4, a);
+            Assert.IsNotNull(await store.TryGetAsync(4));
+
+            await store.RemoveAsync(4);
+            Assert.IsNull(await store.TryGetAsync(4));
+        }
+
+        [TestMethod]
+        public async Task Remove_Unknown()
+        {
+            var store = Store;
+            await store.RemoveAsync(5);
+        }
+
+        [TestMethod]
+        public async Task Length()
+        {
+            var store = Store;
+            await store.PutAsync(6, a);
+            var length = await store.LengthAsync(6);
+            Assert.IsTrue(length.HasValue);
+            Assert.AreNotEqual(0, length.Value);
+        }
+
+        [TestMethod]
+        public async Task Length_Unknown()
+        {
+            var store = Store;
+            var length = await store.LengthAsync(7);
+            Assert.IsFalse(length.HasValue);
+        }
+    }
+}

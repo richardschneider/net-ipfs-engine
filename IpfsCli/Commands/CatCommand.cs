1c2c10a7c794efb5028d4d86c070e9bc47ed5540
Richard Schneider <makaretu@gmail.com>
2018-10-28 18:13:56 +1300

    feat(csipfs): cat command

--- IpfsCli/Commands/CatCommand.cs
+++ IpfsCli/Commands/CatCommand.cs
@@ -1,21 +1,36 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Ipfs.Cli
 {
-    [Command(Description = "Show IPFS data [WIP]")]
+    [Command(Description = "Show IPFS file data")]
     class CatCommand : CommandBase
     {
         [Argument(0, "ref", "The IPFS path to the data")]
-        public string Path { get; set; }
+        [Required]
+        public string IpfsPath { get; set; }
 
         [Option("-o|--offset", Description = "Byte offset to begin reading from")]
         public long Offset { get; set; }
 
         [Option("-l|--length", Description = "Maximum number of bytes to read")]
         public long Length { get; set; }
+
+        Program Parent { get; set; }
+
+        protected override async Task<int> OnExecute(CommandLineApplication app)
+        {
+            using (var stream = await Parent.CoreApi.FileSystem.ReadFileAsync(IpfsPath, Offset, Length))
+            {
+                var stdout = Console.OpenStandardOutput();
+                await stream.CopyToAsync(stdout);
+            }
+            return 0;
+        }
 
     }
 }

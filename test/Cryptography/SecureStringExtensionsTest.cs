cc1f8a60d4b19a8b716ff94518c92d9a0e263c4d
Richard Schneider <makaretu@gmail.com>
2019-04-02 20:08:30 +1300

    feat(IpfsEngine): allow the passphrase to be a SecureString

---
+++ test/Cryptography/SecureStringExtensionsTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipfs.Engine.Cryptography
+{
+    [TestClass]
+    public class SecureStringExtensionsTest
+    {
+        [TestMethod]
+        public void UseBytes()
+        {
+            var secret = new SecureString();
+            var expected = new char[] { 'a', 'b', 'c' };
+            foreach (var c in expected) secret.AppendChar(c);
+            secret.UseSecretBytes(bytes =>
+            {
+                Assert.AreEqual(expected.Length, bytes.Length);
+                for (var i = 0; i < expected.Length; ++i)
+                    Assert.AreEqual((int)expected[i], (int)bytes[i]);
+            });
+        }
+
+
+    }
+}

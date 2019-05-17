1528a2a88002c17478b49f8e9d55f136099a34bd
Richard Schneider <makaretu@gmail.com>
2019-05-18 11:49:45 +1200

    feat(IpfsServer): show node ID on home page

--- IpfsServer/Pages/Index.cshtml.cs
+++ IpfsServer/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ipfs.CoreApi;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,12 +13,25 @@ namespace Ipfs.Server.Pages
     /// </summary>
     public class IndexModel : PageModel
     {
+        ICoreApi ipfs;
+
+        /// <summary>
+        ///   Creates a new instance of the controller.
+        /// </summary>
+        public IndexModel(ICoreApi ipfs)
+        {
+            this.ipfs = ipfs;
+        }
+
+        public string NodeId = "foo-bar";
+
         /// <summary>
         ///   Build the model.
         /// </summary>
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            var peer = await ipfs.Generic.IdAsync();
+            NodeId = peer.Id.ToString();
         }
     }
 }
